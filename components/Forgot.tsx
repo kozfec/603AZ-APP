0646f17a4c733129466d7cf164fb77b18cb6bcf6
kozfec <kozfec@gmail.com>
2023-05-19 00:40:30 +0100

    Bit cleaning up

--- components/Forgot.tsx
+++ components/Forgot.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { Keyboard, SafeAreaView, StyleSheet, View } from 'react-native';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Button, Layout, Text, Icon, IconElement, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
+import { ApplicationProvider, Button, Layout, Text, Icon, IconElement, TopNavigation, TopNavigationAction, Input } from '@ui-kitten/components';
 import { Props } from '@ui-kitten/components/devsupport/services/props/props.service';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 
@@ -13,12 +13,15 @@ import React from 'react';
 
 export default function Forgot({ navigation }: Props) {
 
+    const [email, setEmail] = useState('');
+
 /*const BackIcon = (props: any): IconElement => (
     <Icon
         {...props}
         name='arrow-back'
     />
 );*/
+
     
     const BackAction = (): React.ReactElement => (
        // <TopNavigationAction icon={BackIcon } />
@@ -26,23 +29,59 @@ export default function Forgot({ navigation }: Props) {
       );
       
     return (
-        <><TopNavigation accessoryLeft={BackAction} title='Forgot Page' style={styles.barBg} />
-        <Layout style={styles.container}>
-            <Text>Forgot Page</Text>
-            <Button>IM a Button</Button>
-        </Layout></>
+        <Layout style={styles.container} >
+            <TopNavigation accessoryLeft={BackAction} style={styles.barBg} />
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={styles.headerContainer} >
+                    <Text category='h1'>Reset Password</Text>
+                    <Text style={styles.signInLabel} category='s1'>
+                        Please enter your email address
+                    </Text>
+                </View>
+
+                <View style={styles.formContainer}>
+                    <Input //Email Input
+                        placeholder='Email'
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                        autoCapitalize='none'
+                        inputMode='email'
+                    />
+                </View>
+
+                <Button style={styles.signInButton} size='giant'>
+                    Reset Password
+                </Button>
+            </SafeAreaView>
+        </Layout>
     );
 
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container:  {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
         backgroundColor: "black",
-    },
+      },
+      
     barBg: {
         backgroundColor: "black"
-    }
+    },
+    headerContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: 216,
+      },
+      formContainer: {
+        flex: 1,
+        marignTop: 32,
+        paddingHorizontal: 10,
+      },
+      signInLabel: {
+        marginTop: 16,
+      },
+      signInButton: {
+        marginVertical: 12,
+        marginHorizontal: 16,
+      },
 });
--- components/Login.tsx
+++ components/Login.tsx
@@ -79,6 +79,7 @@ export default function Login({ navigation }: Props){
               value={email}
               onChangeText={(text) => setEmail(text)}
               autoCapitalize='none'
+              inputMode='email'
             />
 
             <Input //Password Input
